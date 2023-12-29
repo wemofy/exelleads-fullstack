323208c786c35b41d366fff866769120ed3087b1
Akshay Waghmare <iec2022117@iiita.ac.in>
2023-12-29 19:37:28 +0530

    whatsapp-plugin

--- src/MainWrapper.jsx
+++ src/MainWrapper.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import store, { persistor } from './redux/store';
@@ -7,6 +8,8 @@ import store, { persistor } from './redux/store';
 const MainWrapper = ({ children }) => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+    <FloatingWhatsApp phoneNumber ="+919373365368" accountName="Exelleads" chatMessage="Hey there 👋, Welcome to Excelleads🚀, How can we help you?"/>
+
       {children}
     </PersistGate>
   </Provider>
