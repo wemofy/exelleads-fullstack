9ae5b044a04c4d0ccdc4af166d46a4733dfdff69
Akshay Waghmare <iec2022117@iiita.ac.in>
2023-12-30 23:12:54 +0530

    sign-out

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -74,7 +74,7 @@ const NavBar = () => {
                     </Link>
                   </li>
                   <li>
-                    <Link to="/">
+                    <Link to="/auth/login">
                       <button
                         className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                         onClick={handleSignout}
@@ -184,7 +184,7 @@ const NavBar = () => {
 
                         {info && info.data ? (
                           <li>
-                            <Link to="/">
+                            <Link to="/auth/login">
                               <button
                                 className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
                                 onClick={handleSignout}
