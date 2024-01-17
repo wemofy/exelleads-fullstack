8a4a714c5c9f8cb2e0b8cc5b1970bcd620d580c6
Akshay Waghmare <iec2022117@iiita.ac.in>
2024-01-17 11:26:53 +0530

    check

--- src/components/FaqSection/FaqSection.jsx
+++ src/components/FaqSection/FaqSection.jsx
@@ -12,7 +12,8 @@ const FaqSection = ({ faq, index, toggleFAQ }) => {
     {/* Title */}
     <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
       <h2 className="text-2xl font-bold md:text-3xl md:leading-tight text-gray-800 dark:text-gray-200">
-        Frequently Asked Questions
+      Terms and Conditions
+
       </h2>
     </div>
     {/* End Title */}
@@ -21,64 +22,130 @@ const FaqSection = ({ faq, index, toggleFAQ }) => {
       <div className="grid sm:grid-cols-2 gap-6 md:gap-12">
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-            Can I cancel at anytime?
+          Services Offered:
           </h3>
           <p className="mt-2 text-gray-600 dark:text-gray-400">
-            Yes, you can cancel anytime no questions are asked while you cancel
-            but we would highly appreciate if you will give us some feedback.
+          We, Exel Leads, specialize in providing lead generation services online, assisting businesses
+in finding unlimited leads to grow their ventures. Our services includes leads generation through online with our
+specific searches from different sources
           </p>
         </div>
         {/* End Col */}
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-            My team has credits. How do we use them?
+          Client Responsibilities:
           </h3>
           <p className="mt-2 text-gray-600 dark:text-gray-400">
-            Once your team signs up for a subscription plan. This is where we
-            sit down, grab a cup of coffee and dial in the details.
+          Clients are expected to provide accurate information and cooperate in the lead
+generation process by supplying necessary data or insights to optimize the service. Any delay or failure to provide
+essential information may affect service delivery timelines.
+3. Lead Quality and Guarantee:	
           </p>
         </div>
         {/* End Col */}
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-            How does Preline's pricing work?
+          Lead Quality and Guarantee:
           </h3>
           <p className="mt-2 text-gray-600 dark:text-gray-400">
-            Our subscriptions are tiered. Understanding the task at hand and
-            ironing out the wrinkles is key.
+          While we strive to provide high-quality leads, we cannot guarantee the
+conversion of leads into paying customers. We commit to delivering leads that meet our quality standards, sourced
+through legitimate means and validated to the best of our abilities.
           </p>
         </div>
         {/* End Col */}
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-            How secure is Preline?
+          Data Confidentiality and Usage:
           </h3>
           <p className="mt-2 text-gray-600 dark:text-gray-400">
-            Protecting the data you trust to Preline is our first priority. This
-            part is really crucial in keeping the project in line to completion.
+          All client information, data, and leads obtained during our services will be
+handled with the utmost confidentiality. However, we reserve the right to use aggregated, anonymized data for
+analytical or marketing purposes.
+
           </p>
         </div>
         {/* End Col */}
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-            Do you offer discounts?
+          Service Modifications and Termination:
           </h3>
           <p className="mt-2 text-gray-600 dark:text-gray-400">
-            We've built in discounts at each tier for teams. The time has come
-            to bring those ideas and plans to life.
+          We reserve the right to modify our services, terms, or pricing with
+prior notice. Clients have the right to terminate services based on agreed-upon terms, subject to any applicable
+cancellation fees.
+
           </p>
         </div>
         {/* End Col */}
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
-            What is your refund policy?
+          Limitation of Liability:
           </h3>
           <p className="mt-2 text-gray-600 dark:text-gray-400">
-            We offer refunds. We aim high at being focused on building
-            relationships with our clients and community.
+          While we make every effort to provide accurate and reliable services, we shall not be
+held liable for any direct or indirect damages, losses, or disruptions incurred by clients using our services
           </p>
         </div>
         {/* End Col */}
+      </div>
+      {/* End Grid */}
+    </div>
+  </div>
+
+  <hr />
+  <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+    {/* Title */}
+    <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+      <h2 className="text-2xl font-bold md:text-3xl md:leading-tight text-gray-800 dark:text-gray-200">
+      Payment:
+
+
+      </h2>
+    </div>
+    {/* End Title */}
+    <div className="max-w-5xl mx-auto">
+      {/* Grid */}
+      <div className="grid sm:grid-cols-2 gap-6 md:gap-12">
+        <div>
+          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
+          Payment Terms:
+          </h3>
+          <p className="mt-2 text-gray-600 dark:text-gray-400">
+          Our payment terms are outlined as follows Specify payment terms such as upfront payments,
+recurring billing, etc. Payments are due within 30 days of invoicing unless otherwise agreed upon.
+          </p>
+        </div>
+        {/* End Col */}
+        <div>
+          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
+          Pricing Structure
+          </h3>
+          <p className="mt-2 text-gray-600 dark:text-gray-400">
+          Our pricing structure is based on subscription model, per-lead pricing
+          </p>
+        </div>
+        {/* End Col */}
+        <div>
+          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
+          Payment Methods:
+          </h3>
+          <p className="mt-2 text-gray-600 dark:text-gray-400">
+          Accepted payment methods include UPI,Debit Cards, Credit Cards. All payments should be made in the designated currency.
+          </p>
+        </div>
+        {/* End Col */}
+        <div>
+          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
+          Refund Policy
+          </h3>
+          <p className="mt-2 text-gray-600 dark:text-gray-400">
+          No Refunds are provided please contact us for more details on the given email id or whatsapp
+number.
+
+          </p>
+        </div>
+      
       </div>
       {/* End Grid */}
     </div>
--- src/components/HomeP/HomeP.jsx
+++ src/components/HomeP/HomeP.jsx
@@ -25,20 +25,21 @@ const HomeP = () => {
         <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
           <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
             <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-              LeadFinder
+              Exelleads
             </p>
             <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-              Everything Lead
+            Empowering Businesses 
+
               <br className="hidden md:block" />
-              you can imagine
+              
               <span className="inline-block text-deep-purple-accent-400">
-                is here!!!
+              with Unlimited Leads!!
               </span>
             </h2>
             <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae. explicabo.
+            At Exel Leads, we are passionate about empowering businesses to thrive by providing unparalleled lead generation
+solutions. With a steadfast commitment to excellence and innovation, we specialize in finding unlimited business
+leads that drive growth and success.
             </p>
             <div className="flex items-center">
               <Link
@@ -97,6 +98,98 @@ const HomeP = () => {
       </div>
 
       <hr />
+      <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+    <div>
+      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+       Exelleads
+      </p>
+    </div>
+    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+      <span class="relative inline-block">
+        <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+          <defs>
+            <pattern id="18302e52-9e2a-4c8e-9550-0cbb21b38e55" x="0" y="0" width=".135" height=".30">
+              <circle cx="1" cy="1" r=".7"></circle>
+            </pattern>
+          </defs>
+          <rect fill="url(#18302e52-9e2a-4c8e-9550-0cbb21b38e55)" width="52" height="24"></rect>
+        </svg>
+        <span class="relative"></span>
+      </span>
+      Empowering Businesses with Unlimited Leads
+
+    </h2>
+    <p class="text-base text-gray-700 md:text-lg">
+    </p>
+  </div>
+  <div class="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
+    <div class="flex flex-col justify-between p-5 border rounded shadow-sm">
+      <div>
+        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+          </svg>
+        </div>
+        <h6 class="mb-2 font-semibold leading-5">Our Mission:</h6>
+        <p class="mb-3 text-sm text-gray-900">
+        Our mission is clear: to be the catalyst for your business's expansion. We understand that a constant
+stream of quality leads is the lifeblood of any successful enterprise. Hence, our focus is on delivering customized
+and scalable lead generation strategies that propel your business forward.
+        </p>
+      </div>
+      <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+    </div>
+    <div class="flex flex-col justify-between p-5 border rounded shadow-sm">
+      <div>
+        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+          </svg>
+        </div>
+        <h6 class="mb-2 font-semibold leading-5">What Sets Us Apart:</h6>
+        <p class="mb-3 text-sm text-gray-900">
+        At the core of our approach lies a dedication to harnessing cutting-edge technology coupled
+with deep industry insights. Our team comprises experts well-versed in the nuances of lead generation, utilizing
+state-of-the-art tools and methodologies to unearth potential leads across diverse industries.
+        </p>
+      </div>
+      <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+    </div>
+    <div class="flex flex-col justify-between p-5 border rounded shadow-sm">
+      <div>
+        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+          </svg>
+        </div>
+        <h6 class="mb-2 font-semibold leading-5">How We Help You Grow:	</h6>
+        <p class="mb-3 text-sm text-gray-900">
+        We believe that the growth of your business is our success metric. By leveraging our
+expertise in finding leads online, we provide you with access to an extensive pool of potential Our focus is not just
+on quantity but on the quality and relevance of the leads we deliver.
+        </p>
+      </div>
+      <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+    </div>
+    <div class="flex flex-col justify-between p-5 border rounded shadow-sm">
+      <div>
+        <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+          <svg class="w-12 h-12 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+          </svg>
+        </div>
+        <h6 class="mb-2 font-semibold leading-5">Get Started with Exel Leads:</h6>
+        <p class="mb-3 text-sm text-gray-900">
+        Are you ready to take your business to new heights? Partner with us and unlock a
+world of unlimited business leads. Let's embark on this journey together towards sustainable growth and prosperity        </p>
+      </div>
+      <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">Learn more</a>
+    </div>
+  </div>
+</div>
+
+<hr/> 
 
       <HomePlans />
 
